78c418034d7274baf57c020b3d3e54712588f900
Pham Son <sonpham@radtechcompany.com>
2020-04-27 23:11:34 +0700

    init Escape Plan Project

---
+++ EscapePlan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NightSky
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
