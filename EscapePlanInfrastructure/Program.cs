7868ee65d868cfa99a5133462af95ee067d6ba23
Pham Son <sonpham@radtechcompany.com>
2020-04-27 23:42:58 +0700

    init project structure

---
+++ EscapePlanInfrastructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EscapePlanInfrastructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ plan.md
@@ -0,0 +1,7 @@
+
+I)		Admin Site: React + DotnetCore API:	
+			1) Theme: https://coreui.io/demo/3.1.0/dark/#base/breadcrumb.html
+			2) Library:
+			3) Yarn installer
+II)		Movie Site: Dotnet MVC
+III)	Database: MongoDB
